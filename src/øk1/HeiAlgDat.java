26c149c8d30129328288499087fbc4d8f0f46c43
Thea <thealp@altiboxmail.no>
2020-08-18 10:29:21 +0200

    Initial commit

---
+++ src/øk1/HeiAlgDat.java
@@ -0,0 +1,7 @@
+package øk1;
+
+public class HeiAlgDat {
+    public static void main(String[]args){
+        System.out.println("Hei AlgDat!");
+    }
+}
---
+++ src/øk1/oppg.java
@@ -0,0 +1,8 @@
+package øk1;
+
+public class oppg {
+   public static void main(String[]args){
+       System.out.print("Hei AlgDat");
+   }
+
+}
